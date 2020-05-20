7805105c8e71d4f64123ccfa6f56c61cda8a2863
PJZ9n <pjz9n@yahoo.co.jp>
2020-05-20 17:14:04 +0900

    Complete

--- src/PJZ9n/GiveMoney/Main.php
+++ src/PJZ9n/GiveMoney/Main.php
@@ -23,9 +23,47 @@ declare(strict_types=1);
 
 namespace PJZ9n\GiveMoney;
 
+use PJZ9n\MoneyConnector\Connectors\EconomyAPI;
+use PJZ9n\MoneyConnector\Connectors\MixCoinSystem;
+use PJZ9n\MoneyConnector\Connectors\MoneySystem;
+use PJZ9n\MoneyConnector\MoneyConnector;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
+use RuntimeException;
 
-class Main extends PluginBase
+class Main extends PluginBase implements Listener
 {
-    //
+    /** @var MoneyConnector */
+    private $moneyConnector;
+    
+    public function onEnable(): void
+    {
+        $this->saveDefaultConfig();
+        $moneyAPI = $this->getConfig()->get("use-economy-api");
+        switch ($moneyAPI) {
+            case "EconomyAPI":
+                $this->moneyConnector = new EconomyAPI();
+                break;
+            case "MixCoinSystem":
+                $this->moneyConnector = new MixCoinSystem();
+                break;
+            case "MoneySystem":
+                $this->moneyConnector = new MoneySystem();
+                break;
+            default:
+                throw new RuntimeException("API \"{$moneyAPI}\" is not supported");
+        }
+        
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    
+    public function onPlayerJoin(PlayerJoinEvent $event): void
+    {
+        $player = $event->getPlayer();
+        $amount = (int)$this->getConfig()->get("money-amount");
+        $unit = $this->moneyConnector->getMonetaryUnit();
+        $player->sendMessage("Hey {$player->getName()}! we have a gift for you! Given {$unit}{$amount} !");
+        $this->moneyConnector->addMoney($player, $amount);
+    }
 }
